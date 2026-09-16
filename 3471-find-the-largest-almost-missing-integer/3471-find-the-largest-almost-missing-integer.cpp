c25464e4b7f5c9c782a34b8a1aaa3794d2b73fa2
Chetan kolhe <158244717+ChetankolheAi@users.noreply.github.com>
2026-09-16 20:17:49 +0530

    Time: 3 ms (44.7%), Space: 29.6 MB (25.59%) - LeetHub

---
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int FindMax(vector<int>&nums , int k){
+        int maxi = INT_MIN;
+            for(int i : nums){
+                maxi = max(i , maxi);
+            }
+        return maxi;
+    }
+    int largestInteger(vector<int>& nums, int k) {
+        if(k==nums.size()){
+            return FindMax(nums,k);
+        }
+        else if(k==1){
+            unordered_map<int,int>Freq;
+            for(int i : nums){
+                Freq[i]++;
+            }
+            int maxi = INT_MIN;
+            for(int i : nums){
+                if(Freq[i]==1){
+                    cout<<i<<endl;
+                    maxi = max(i , maxi);
+                }
+            }
+            if(maxi ==INT_MIN){
+                return -1;
+            }
+        
+            return maxi;
+            
+           
+        }
+        
+        unordered_map<int,int>Freq;
+        for(int i : nums){
+            Freq[i]++;
+        }
+        int start = nums[0];
+        int end = nums[nums.size()-1];
+
+        if(Freq[start]==1){
+            if(Freq[end]==1){
+                return max(start , end);
+            }
+            else{
+                return start;
+            }
+        }
+        else if(Freq[end]==1){
+            return end;
+        }
+        if(Freq[start]==nums.size() && nums.size()==k){
+            return start;
+        }
+        return -1;
+
+    }
+};
