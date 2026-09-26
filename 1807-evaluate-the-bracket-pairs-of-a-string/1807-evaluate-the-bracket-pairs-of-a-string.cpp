4e6772c0a828f8f7a54144225bfe1380f8b0601c
Chetan kolhe <158244717+ChetankolheAi@users.noreply.github.com>
2026-09-26 17:39:59 +0530

    Time: 884 ms (5.3%), Space: 144.9 MB (6.23%) - LeetHub

---
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+        unordered_map<string, string>know;
+        for(auto i : knowledge){
+            know[i[0]] = i[1];
+        }
+
+        for(auto  i : know){
+            cout<<i.first<<","<<i.second<<endl;
+        }
+
+        int j = 0;
+        bool flag = false;
+        string subString = "";
+        string ans ="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                flag = true;
+                continue;
+            }
+            
+            else if(s[i]==')'){
+            
+                auto it = know.find(subString);
+
+                if (it != know.end()) {
+                    ans+=it->second;   
+                } else {
+                    ans+='?';
+                }
+                flag = false;
+                subString = "";
+                continue;
+            }
+        
+            if(flag){
+                subString+=s[i];
+                continue;
+            }
+            else if(!flag){
+                ans+=s[i];
+            }
+        }
+        return ans;
+    }
+};
